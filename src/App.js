89394daa9fee5ecebd067a63c72b1034febddadf
sq1fya <rgpszczecin@gmail.com>
2018-12-04 20:05:31 +0100

    WIP: section nav - set color from project and add text

--- src/App.js
+++ src/App.js
@@ -1,31 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import logo from './logo.svg';
-import './Logo.scss';
-import './main.scss';
+import "./Logo.scss";
+import "./main.scss";
 
 class App extends Component {
   render() {
     return (
       <section className="container-fluid image-lukasz">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <div className="row">
-            <div className="container">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <div className="row">
+          <div className="container">
             <div className="row">
               <header>
-          
-                
-                  <div className="col"></div>
-              
-          
+                <div className="col" />
                 <nav>
-
+                  <ul>
+                    <li>Umiejętności</li>
+                    <li>Kontakt</li>
+                  </ul>
                 </nav>
               </header>
             </div>
             <div className="row">
-              <div className="col-5">
-
+              <div className="left-box col-5">
+                <h5 className="title">Jestę frontendowcę</h5>
+                <div className="inside-text">
+                  <h3>
+                    <strong>Cześć!</strong>
+                  </h3>
+                  <p>
+                    Jestem web developerem i rozwijam się na ścieżce javascriptu
+                  </p>
+                  <hr />
+                  <h3>
+                    <strong>Mocne strony:</strong>
+                  </h3>
+                  <ul>
+                    <li>- React</li>
+                    <li>- Javascript</li>
+                    <li>- RWD</li>
+                  </ul>
+                </div>
               </div>
+              <div className="js"><span><strong className="js-text">JS</strong></span></div>
+            </div>
+            <div className="right-box col-5">
+            
             </div>
           </div>
         </div>
