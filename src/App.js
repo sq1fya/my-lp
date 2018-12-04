b7e89d033c80fdec49f4d9878ed3e9d24ade75c0
sq1fya <rgpszczecin@gmail.com>
2018-12-04 18:43:07 +0100

    WIP: section nav

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './Logo.scss';
 import './main.scss';
 
 class App extends Component {
   render() {
     return (
-      <div>
-          <img src={logo} className="App-logo" alt="logo" />
-        <header>
-          <div class="bg-danger one"></div>
-        </header>
-      </div>
+      <section className="container-fluid image-lukasz">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <div className="row">
+            <div className="container">
+            <div className="row">
+              <header>
+          
+                
+                  <div className="col"></div>
+              
+          
+                <nav>
+
+                </nav>
+              </header>
+            </div>
+            <div className="row">
+              <div className="col-5">
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
 }
