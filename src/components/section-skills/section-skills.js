ac48ca33b68ea3dfdc334f1a9a9f8a8e1aebbca5
sq1fya <rgpszczecin@gmail.com>
2018-12-07 15:26:30 +0100

    add progress bar and two col

--- src/components/section-skills/section-skills.js
+++ src/components/section-skills/section-skills.js
@@ -11,7 +11,7 @@ class SectionSkills extends Component {
               <h3>Umiejętności</h3>
             </div>
             <div className="row">
-              <div className="left-box col-4">
+              <div className="left-box col-md-4">
                 <div className="title-box justify-content-center">
                   <h6 className="title">Jestę juniorę</h6>
                 </div>
@@ -29,8 +29,7 @@ class SectionSkills extends Component {
                   </p>
                 </div>
               </div>
-              <div className="col ">
-              <div className="skills-box col bg-danger">
+              <div className="skills-box col-md-4 bg-success">
                 <div className="skills-names col">
                   <div className="skill-name row">
                     <p>React</p>
@@ -55,12 +54,77 @@ class SectionSkills extends Component {
                   </div>
                 </div>
               </div>
-              <div className="col-5 bg-info">
-              <div className="skills-lines col">
-                <div className="skill-line"> </div>
-              </div>
-              </div>
-              
+              <div className="skills-lines col-md-4 bg-info">
+                <div className="progress">
+                  <div
+                    class="progress-bar progress-bar-striped progress-bar-animated"
+                    role="progressbar"
+                    aria-valuenow="40"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                    style={{ width: "30%" }}
+                  />
+                </div>
+                <div className="progress">
+                  <div
+                    class="progress-bar progress-bar-striped progress-bar-animated bg-success"
+                    role="progressbar"
+                    aria-valuenow="40"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                    style={{ width: "70%" }}
+                  />
+                </div>
+                <div className="progress">
+                  <div
+                    class="progress-bar progress-bar-striped progress-bar-animated bg-info"
+                    role="progressbar"
+                    aria-valuenow="40"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                    style={{ width: "20%" }}
+                  />
+                </div>
+                <div className="progress">
+                  <div
+                    class="progress-bar progress-bar-striped progress-bar-animated bg-warning"
+                    role="progressbar"
+                    aria-valuenow="40"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                    style={{ width: "80%" }}
+                  />
+                </div>
+                <div className="progress">
+                  <div
+                    class="progress-bar progress-bar-striped progress-bar-animated bg-success"
+                    role="progressbar"
+                    aria-valuenow="40"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                    style={{ width: "40%" }}
+                  />
+                </div>
+                <div className="progress">
+                  <div
+                    class="progress-bar progress-bar-striped progress-bar-animated bg-primary"
+                    role="progressbar"
+                    aria-valuenow="40"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                    style={{ width: "50%" }}
+                  />
+                </div>
+                <div className="progress">
+                  <div
+                    class="progress-bar progress-bar-striped progress-bar-animated bg-danger"
+                    role="progressbar"
+                    aria-valuenow="40"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                    style={{ width: "90%" }}
+                  />
+                </div>
               </div>
             </div>
           </div>
