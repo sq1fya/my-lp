a049a899bd7b8bc4561e7e8aec326fa117cd55d0
sq1fya <rgpszczecin@gmail.com>
2018-12-07 10:30:45 +0100

    review change

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ import "./main.scss";
 class App extends Component {
   render() {
     return (
-      <section className="container-fluid image-lukasz">
-        <div className="row ">
+      <section className="section-about-me background-image container-fluid ">
+        <div className="row">
           <div className="container">
             <div className="row justify-content-end">
               <header>
-                <div className="col ">
-                  <nav className="nav-bar ">
-                    <ul className="nav ">
+                <div className="col">
+                  <nav className="nav-bar">
+                    <ul className="nav">
                       <li className="nav-item">
                         <button className="nav-link" href="#">
                           Umiejętności
@@ -43,24 +43,20 @@ class App extends Component {
                   <hr className="border-white" />
                   <h3>Mocne strony:</h3>
                   <ul className="list-group">
-                    <li className="">- React</li>
-                    <li className="">- Javascript</li>
-                    <li className="">- RWD</li>
+                    <li>- React</li>
+                    <li>- Javascript</li>
+                    <li>- RWD</li>
                   </ul>
                 </div>
               </div>
-              <div className="js sticky-top mr-auto p-3">
-                JS
-              </div>
+              <div className="js sticky-top mr-auto p-3">JS</div>
             </div>
             <div className="right-box col-6 float-right position-relative">
               <div className="react-logo float-right position-absolute">
                 <img src={logo} className="App-logo sticky-top" alt="logo" />
               </div>
               <div className="title-box justify-content-center">
-                <h6 className="title sticky-top">
-                  Jestę developerę
-                </h6>
+                <h6 className="title sticky-top">Jestę developerę</h6>
               </div>
               <div className="inside-text">
                 <h3>Łukasz Górecki</h3>
