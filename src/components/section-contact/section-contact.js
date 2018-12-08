5f5d0d00d1d4ab66f9b69a5d332a61c6c96b8d20
sq1fya <rgpszczecin@gmail.com>
2018-12-08 21:57:08 +0100

    add email and name placeholder

--- src/components/section-contact/section-contact.js
+++ src/components/section-contact/section-contact.js
@@ -16,39 +16,26 @@ class SectionContact extends Component {
             </div>
             <form>
               <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
+                <label for="validationDefault01"></label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="validationDefault01"
+                  placeholder="Imię i nazwisko / Firma"
+                />
+              </div>
+              <div class="form-group">
+                <label for="exampleInputEmail1"></label>
                 <input
                   type="email"
                   class="form-control"
                   id="exampleInputEmail1"
                   aria-describedby="emailHelp"
-                  placeholder="Enter email"
+                  placeholder="Email"
                 />
-                <small id="emailHelp" class="form-text text-muted">
-                  We'll never share your email with anyone else.
-                </small>
-              </div>
-              <div class="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input
-                  type="password"
-                  class="form-control"
-                  id="exampleInputPassword1"
-                  placeholder="Password"
-                />
-              </div>
-              <div class="form-check">
-                <input
-                  type="checkbox"
-                  class="form-check-input"
-                  id="exampleCheck1"
-                />
-                <label class="form-check-label" for="exampleCheck1">
-                  Check me out
-                </label>
               </div>
               <button type="submit" class="btn btn-primary">
-                Submit
+                Wyślij
               </button>
             </form>
           </div>
