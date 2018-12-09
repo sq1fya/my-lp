7c9e51347062747a701a369f9b8fd72499f46bd0
sq1fya <rgpszczecin@gmail.com>
2018-12-09 08:17:17 +0100

    section footer style done

--- src/components/section-footer/section-footer.js
+++ src/components/section-footer/section-footer.js
@@ -8,8 +8,20 @@ class SectionFooter extends Component {
         <div className="row ">
           <div className="container" style={{ height: "130px" }}>
             <div className="info-box row justify-content-between  ">
-              <div className="col-3"><h5>Łukasz Górecki</h5> React junior developer</div>
-              <div className="col-4">tel: 515 115 750 mail: lukasz.gorecki@hotmail.com</div>
+              <div className="info-box--name col-3">
+                <p>Łukasz Górecki</p>
+                <p>React junior developer</p>
+              </div>
+              <div className="info-box--contact col-4">
+                <p>
+                  <spam>tel: </spam>
+                  <span>515 115 750</span>
+                </p>
+                <p>
+                  <span>mail: </span>
+                  <span>lukasz.gorecki@hotmail.com</span>
+                </p>
+              </div>
             </div>
           </div>
         </div>
