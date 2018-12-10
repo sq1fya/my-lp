19deb1d366afc8a85cc779dc7b16d4490f558c3e
sq1fya <rgpszczecin@gmail.com>
2018-12-10 13:58:04 +0100

    WIP section about me right box style

--- src/components/section-about-me/section-about-me.js
+++ src/components/section-about-me/section-about-me.js
@@ -7,56 +7,61 @@ class SectionAboutMe extends Component {
       <section className="section-about-me background-image container-fluid">
         <div className="row">
           <div className="container">
-              <header>
-                  <nav className="navbar nav-bar justify-content-end">
-                    <ul className="nav position-fixed">
-                      <li className="nav-item">
-                        <button className="nav-link" href="#">
-                          Umiejętności
-                        </button>
-                      </li>
-                      <li>
-                        <button className="nav-link" href="#">
-                          Kontakt
-                        </button>
-                      </li>
-                    </ul>
-                  </nav>
-              </header>
+            <header>
+              <nav className="navbar nav-bar justify-content-end">
+                <ul className="nav">
+                  <li className="nav-item">
+                    <button className="nav-link" href="#">
+                      Umiejętności
+                    </button>
+                  </li>
+                  <li>
+                    <button className="nav-link" href="#">
+                      Kontakt
+                    </button>
+                  </li>
+                </ul>
+              </nav>
+            </header>
             <div className="row d-flex align-items-end">
               <div className="left-box col-4">
                 <div className="title-box justify-content-center">
                   <h6 className="title">Jestę frontendowcę</h6>
                 </div>
                 <div className="inside-text">
-                <div className="inside-text--box">
-                  <h3>Cześć!</h3>
-                  <p>
-                    Jestem web developerem i&nbsp;rozwijam się na ścieżce
-                    javascriptu
-                  </p>
-                  <hr className="border-white" />
-                  <h3>Mocne strony:</h3>
-                  <ul className="list-group">
-                    <li>- React</li>
-                    <li>- Javascript</li>
-                    <li>- RWD</li>
-                  </ul>
+                  <div className="inside-text--box">
+                    <h3>Cześć!</h3>
+                    <p>
+                      Jestem web developerem i&nbsp;rozwijam się na ścieżce
+                      javascriptu
+                    </p>
+                    <hr className="border-white" />
+                    <h3>Mocne strony:</h3>
+                    <ul className="list-group">
+                      <li>- React</li>
+                      <li>- Javascript</li>
+                      <li>- RWD</li>
+                    </ul>
                   </div>
                 </div>
               </div>
-              <div className="js sticky-top"><p>JS</p></div>
+              <div className="js sticky-top">
+                <p>JS</p>
+              </div>
             </div>
-            <div className="right-box col-6 float-right position-relative">
-              <div className="react-logo float-right position-absolute">
-                <img src={logo} className="App-logo" alt="logo" />
-              </div>
-              <div className="title-box justify-content-center">
-                <h6 className="title sticky-top">Jestę developerę</h6>
-              </div>
-              <div className="inside-text">
-                <h3>Łukasz Górecki</h3>
-                <p className="mb-auto">Junior frontend developer</p>
+            <div className="right-box col-6 float-right">
+
+              <div className="right-box--main">
+                  <div className="title-box">
+                    <h6 className="title">Jestę developerę</h6>
+                  </div>
+                <div className="inside-text">
+                  <h3>Łukasz Górecki</h3>
+                  <p className="mb-auto">Junior frontend developer</p>
+                  {/* <div className="react-logo float-right">
+                  <img src={logo} className="App-logo" alt="logo" />
+                </div> */}
+                </div>
               </div>
             </div>
           </div>
