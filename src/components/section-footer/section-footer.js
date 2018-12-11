fea6b246df06adcd69827320ca2c80550a732dc9
sq1fya <rgpszczecin@gmail.com>
2018-12-11 19:40:40 +0100

    Style section contact

--- src/components/section-footer/section-footer.js
+++ src/components/section-footer/section-footer.js
@@ -8,12 +8,12 @@ class SectionFooter extends Component {
         <div className="row ">
           <div className="container">
             <div className="info-box text-white row justify-content-lg-between flex-lg-nowrap flex-md-wrap flex-md-column flex-lg-row align-items-md-center">
-              <div className="info-box--name col-lg-3 col-md-6 text-lg-left ">
+              <div className="info-box--name col-lg-3 col-md-6 col-sm-6 text-lg-left ">
                 <p>Łukasz Górecki</p>
                 <p>React junior developer</p>
                 <hr className="d-lg-none d-md-block w-75 float-left" />
               </div>
-              <div className="info-box--contact col-lg-5 col-md-6 text-lg-right">
+              <div className="info-box--contact col-lg-5 col-md-6  text-lg-right">
                 <p>
                   <spam>tel: </spam>
                   <span><a className="text-white" href="tel:+48-515-115-750">515 115 750</a></span>
