83785e3133f9b477210a2fcbc136706eb64c1ec9
sq1fya <rgpszczecin@gmail.com>
2018-12-16 16:16:18 +0100

    WIP: Navbar add to section about me

--- src/components/section-about-me/section-about-me.js
+++ src/components/section-about-me/section-about-me.js
@@ -6,6 +6,43 @@ class SectionAboutMe extends Component {
   render() {
     return (
       <section className="position-relative section-about-me background-image container-fluid h-auto">
+        
+        <div className="container-fluid">
+        <div className="row">
+          <div className="container col-lg-9">
+            <header className="container p-0">
+              <nav class="navbar d-block nav-bar navbar-expand-lg navbar-light p-0">
+                <button
+                  class="navbar-toggler"
+                  type="button"
+                  data-toggle="collapse"
+                  data-target="#navbarText"
+                  aria-controls="navbarText"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation"
+                >
+                  <span class="navbar-toggler-icon" />
+                </button>
+                <div class="collapse navbar-collapse" id="navbarText">
+                  <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                      <a class="nav-link" href="#">
+                        Umiejętności <span class="sr-only">(current)</span>
+                      </a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">
+                        Kontakt
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </nav>
+            </header>
+          </div>
+        </div>
+      </div>
+
         <div
           className="background-image-media row"
           style={{
