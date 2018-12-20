31396e7ef7250b3d4853e679258448403e3f2480
sq1fya <rgpszczecin@gmail.com>
2018-12-20 10:50:53 +0100

    WIP: add to form alertify

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,114 +1,82 @@
 import React from "react";
 import axios from "axios"; // For making client request.
+import alertify from "../../../node_modules/alertifyjs/build/alertify.min.js";
+import "../../../node_modules/alertifyjs/build/css/alertify.min.css";
+import "../../../node_modules/alertifyjs/build/css/themes/default.min.css";
 
-const BASE_URL = 'https://formcarry.com/s/5QT28VUaMjd'
-
+const BASE_URL = "https://formcarry.com/s/5QT28VUaMjd";
 
 class Form extends React.Component {
-
-  // constructor(props){
-  //   super(props);
-  //   this.state = {name: "", email: "",message: "", loading: false};
-  // }
-
-  // handleForm = e => {
-  //   axios.post(
-  //     "https://formcarry.com/s/5QT28VUaMjd",
-  //     this.state,
-  //     {headers: {"Accept": "application/json"}}
-  //     )
-  //     .then(function (response) {
-  //       console.log(response);
-  //       this.setState({message: response.data})
-
-  //     })
-  //     .catch(function (error) {
-  //       console.log(error);
-  //       alert('Catch is ignored, sdadsadadsasrs');
-  //     });
-  //         // this.setState({
-  //         //   message: "Wysłanie udane"
-  //         // });
-
-  //   e.preventDefault();
-  // }
-
-  // handleFields = e => this.setState({ [e.target.name]: e.target.value });
-state = {
-  title: '',
-  boody: '',
-  isSubmited: false,
-  error: false
-}
-
-changeHandler = (e) => {
-  this.setState({
-    [e.target.name]: e.target.value
-  })
-}
-
-submitHandler = (e) => {
-  e.preventDefault()
-
-  axios.post(`${BASE_URL}`, {
-    title: this.state.title,
-    body: this.state.body
-  })
-  .then(res => {
-    this.setState({
-      isSubmited: true,
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      boody: "",
+      isSubmited: false,
       error: false
-    })
-  })
-  .catch(error => {
+    };
+  }
+
+  changeHandler = e => {
     this.setState({
-      error: true,
-      isSubmited: false
-    })
-  })
-}
+      [e.target.name]: e.target.value
+    });
+  };
+
+  submitHandler = e => {
+    e.preventDefault();
+
+    axios
+      .post(`${BASE_URL}`, {
+        title: this.state.title,
+        body: this.state.body
+      })
+      .then(res => {
+        this.setState({
+          isSubmited: true,
+          error: false
+        });
+        alertify.set("notifier", "position", "bottom-center");
+        alertify.success("Dzieki, za kontakt ze mna :D");
+      })
+      .catch(error => {
+        this.setState({
+          error: true,
+          isSubmited: false
+        });
+        alertify.set("notifier", "position", "bottom-center");
+        alertify.error("Coś poszło nie tak, sprawdź dane");
+      });
+  };
 
   render() {
     return (
       <div>
-        <form onSubmit={ this.submitHandler}>
-        <input 
-        type="text"
-        className='form-control'
-        name='title'
-        value={ this.state.title } 
-        placeholder='Enter Your Title'
-        onChange={ this.changeHandler}
-        />
-        <input 
-        type="text"
-        className='form-control'
-        name='body'
-        value={ this.state.body } 
-        placeholder='Enter Email'
-        onChange={ this.changeHandler}
-        />
-
-
-        <button type='submit' className='btn btn-success'>SUBMIT</button>
+        <form onSubmit={this.submitHandler}>
+          <input
+            id="textInput"
+            type="text"
+            className="form-control"
+            name="title"
+            value={this.state.title}
+            placeholder="Enter Your Title"
+            onChange={this.changeHandler}
+          />
+          <input
+            type="text"
+            className="form-control"
+            name="body"
+            value={this.state.body}
+            placeholder="Enter Email"
+            onChange={this.changeHandler}
+          />
+          <button type="submit" className="btn btn-success">
+            SUBMIT
+          </button>
         </form>
-        { this.state.isSubmited  && <p>Form Submiter Succes</p> }
-        { this.state.error  && <p>Something Error</p> }
+        {this.state.isSubmited && <p>Form Submiter Succes </p>}
+        {this.state.error && <p>Something Error</p>}
       </div>
-
-      //   <form onSubmit={this.handleForm}>
-      //     <label htmlFor="name">Name</label>
-      //     <input type="text" id="name" name="name" onChange={this.handleFields} />
-
-      //     <label htmlFor="email">Email</label>
-      //     <input type="email" id="email" name="email" onChange={this.handleFields} />
-
-      //     <button type="submit">Send</button>
-      //     <div>
-      //       {this.state.message}
-      //     </div>
-      //   </form>
-      // );
     );
   }
 }
